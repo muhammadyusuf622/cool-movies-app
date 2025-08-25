82041396cdc6c90db0a04c01929b56e0c38d5781
Muhammad Yusuf <yuvsufn@gmail.com>
2025-08-25 18:41:16 +0500

    update

---
+++ app/movie/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MovieDetals = () => {
+  return (
+    <View>
+      <Text>MovieDetals</Text>
+    </View>
+  )
+}
+
+export default MovieDetals
